8983eed99983cdbde0998f2134a6642e54c9c0e2
kayke9 <kaykefioravante190@gmail.com>
2024-11-25 09:43:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'melhor 9 da seleçao?',
+    'endrick bobby charlton🦍🦧🦍🐒🐵🐒🦍🐵🦧🌴🌴'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do brasil é brasilia'
+)
+
+criaCartao(
+    'futebol?',
+    'qual o maior time do brasil',
+    'palmeiras mancha verde  🌴 🌴 💚 💚 🌴 🌴🐽🐷🐖'
+)
+
+criaCartao(
+    'brasil',
+    'qual a maior facção do brasil?',
+    'pcc tudo 3 👌👌(RAI)'
+)
